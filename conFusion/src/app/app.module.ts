093f08ed9493f2c1a59d75aa890aeb426f817b8a
pougilhac <christian.collenot@gmail.com>
2017-12-26 11:27:55 +0100

    Intermédiaire
    Repositionnement dans l'architecture de dishdetail

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -12,7 +12,7 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './menu/dishdetail/dishdetail.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
