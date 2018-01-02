42ecd00012aca9d97ec70e96a328e24dfdbf0509
pougilhac <christian.collenot@gmail.com>
2018-01-02 09:35:10 +0100

    Assignment 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import {DishService} from './services/dish.service';
 import {PromotionService} from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
 
@@ -44,7 +45,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule, FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
