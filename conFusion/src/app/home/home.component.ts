5b53348f7dc5a9b27b140ef0ef4a5d97cae5376f
pougilhac <christian.collenot@gmail.com>
2018-01-04 11:59:05 +0100

    Promise Part 1
    retour immédiat ; then uniquement utilisé

--- conFusion/src/app/home/home.component.ts
+++ conFusion/src/app/home/home.component.ts
@@ -18,14 +18,17 @@ export class HomeComponent implements OnInit {
   promotion: Promotion;
   leader: Leader;
 
-  constructor(private dishservice: DishService,
-    private promotionservice: PromotionService,
-    private leaderservice: LeaderService) { }
+  constructor(private dishService: DishService,
+    private promotionService: PromotionService,
+    private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion();
-    this.leader = this.leaderservice.getFeaturedLeader();
+    this.dishService.getFeaturedDish()
+    .then(dish => this.dish = dish);
+    this.promotionService.getFeaturedPromotion()
+    .then(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader()
+    .then(leader => this.leader = leader);
   }
 
 }
