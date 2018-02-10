2a020cb245c4c6afb981be834475f5083bba4d0a
pougilhac <christian.collenot@gmail.com>
2018-02-10 12:09:12 +0100

    Directives

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { RestangularConfigFactory } from './shared/restConfig';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
